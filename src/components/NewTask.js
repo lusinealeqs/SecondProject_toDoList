169e962d3c643bdb7e81230ee86857fb2f9535f1
lusinealeqs <lusinealeksanyan07@gmail.com>
2020-09-20 10:10:10 +0400

    ToDo list project

---
+++ src/components/NewTask.js
@@ -0,0 +1,56 @@
+import React, { PureComponent } from 'react';
+import { InputGroup, FormControl, Button } from 'react-bootstrap';
+
+
+class NewTask extends PureComponent {
+    state = {
+        inputValue: ''
+    };
+
+    handleChange = (event) => {
+        this.setState({
+            inputValue: event.target.value
+        });
+    };
+
+    handleKeyDown = (event) => {
+        if (event.key === 'Enter') {
+            this.sendValue();
+        }
+    };
+
+    sendValue = () => {
+        const { inputValue } = this.state;
+        if (!inputValue) {
+            return;
+        }
+        this.props.onAdd(inputValue);
+        this.setState({
+            inputValue: ''
+        });
+    }
+
+    render() {
+        return (
+            <InputGroup
+                className="my-3"
+            >
+                <FormControl
+                    value={this.state.inputValue}
+                    onChange={this.handleChange}
+                    onKeyDown={this.handleKeyDown}
+                    placeholder="Input task"
+                    aria-label="Input task"
+                    aria-describedby="basic-addon2"
+                />
+                <InputGroup.Append>
+                    <Button onClick={this.sendValue} variant="outline-light">Add task</Button>
+                </InputGroup.Append>
+            </InputGroup>
+        );
+    }
+
+
+}
+
+export default NewTask;
---
+++ src/components/Task/Task.js
@@ -0,0 +1,65 @@
+import React, { PureComponent } from 'react';
+import { Card, Button } from 'react-bootstrap';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faTrash, faEdit } from '@fortawesome/free-solid-svg-icons';
+import styles from './task.module.css';
+
+class Task extends PureComponent {
+    state = {
+        checked: false,
+    };
+
+    toggleCheckbox = () => {
+        this.setState({
+            checked: !this.state.checked
+        });
+
+        this.props.onCheck();
+    };
+
+    toggleModal = () => {
+        this.setState({
+            showModal: !this.state.showModal
+        });
+    }
+
+    render() {
+        const { data, onRemove, onEdit } = this.props;
+        const { checked } = this.state;
+
+        const cardClasses = ['card', styles.task];
+        if (checked) {
+            cardClasses.push(styles.checked);
+        }
+
+        return (
+            <Card className={cardClasses.join(' ')}>
+                <input
+                    type='checkbox'
+                    className={styles.checkbox}
+                    onClick={this.toggleCheckbox}
+                />
+                <Card.Body>
+                    <Card.Title>Your Task</Card.Title>
+                    <Card.Text>{data.text}</Card.Text>
+                    <Button
+                        className='m-1'
+                        variant="outline-dark"
+                        onClick={onEdit}
+                    >
+                        <FontAwesomeIcon icon={faEdit} />
+                    </Button>
+                    <Button
+                        className='m-1'
+                        variant="outline-dark"
+                        onClick={onRemove(data.id)}
+                    >
+                        <FontAwesomeIcon icon={faTrash} />
+                    </Button>
+                </Card.Body>
+            </Card>
+        );
+    }
+}
+
+export default Task;
