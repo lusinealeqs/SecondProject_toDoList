360552adb33a8a560dafceb57e35af14dff7f823
lusinealeqs <lusinealeksanyan07@gmail.com>
2021-01-13 04:32:33 +0400

    Continued login, added logout, made a new version of aboutPage, created contact page(to be continued), and so on

---
+++ src/components/Pages/Contact/Contact.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import styles from './contact.module.css';
+import { Container, Row, Col } from 'react-bootstrap';
+
+export default function About() {
+    return (
+        <div className={styles.main}>
+            <Container className={styles.mainInner}>
+                <Row className="justify-content-center">
+                    <Col xs={12} sm={8} md={6}>
+                        <div className={styles.inner}>
+                            <h3 className={styles.heading}>Contact Us</h3>
+                            <p>Feel free to get in touch with us whenever you need some help.</p>
+                        </div>
+                    </Col>
+                </Row>
+            </Container>
+
+        </div>
+    )
+}
